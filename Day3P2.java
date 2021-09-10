8ddda347958ba9f96d78f9fe472912bc28a717b2
Dr. Sunil Kumar Jangir <39852522+suniljangir@users.noreply.github.com>
2021-09-10 17:51:21 +0530

    Add files via upload

---
+++ Day3P2.java
@@ -0,0 +1,22 @@
+// Multilevel level inheritance
+
+class A				//Super class
+{
+int X=10;
+}
+class B extends A			//Super class for Day3P2 and sub class for A
+{
+int Y=20;
+}
+class Day3P2 extends B		// sub class for A
+{
+int Z=30;
+	public static void main(String args[])
+	{
+		Day3P2  obj=new Day3P2  ();
+		System.out.println("X = "+obj.X+" Y = "+obj.Y+" Z = "+obj.Z);		// Accesing super class value using sub class object
+	
+	}
+
+
+}
---
+++ Day3P3.java
@@ -0,0 +1,25 @@
+// Hierarchical level inheritance
+
+class A				//Super class
+{
+int X=10;
+}
+class B extends A			// sub class for A
+{
+int Y=20;
+}
+class Day3P3 extends A		// sub class for A	
+{
+int Z=30;
+	public static void main(String args[])
+	{
+		Day3P3  obj=new Day3P3  ();
+		System.out.println("Day3P3 class object => X = "+obj.X+" Z = "+obj.Z);		// Accesing super class value using sub class object
+		B b=new B();
+		System.out.println("B class Object => X = "+b.X+" Y = "+b.Y);			// Accesing super class value using sub class object
+			
+	
+	}
+
+
+}
