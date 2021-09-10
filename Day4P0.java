6f1d926cf6f0df80716d16d815a2ba7062be3506
Dr. Sunil Kumar Jangir <39852522+suniljangir@users.noreply.github.com>
2021-09-10 20:53:08 +0530

    Add files via upload

---
+++ Day4P0.java
@@ -0,0 +1,15 @@
+//create a package TEMP
+package TEMP;
+class Day4P0	
+{
+
+
+	public static void main(String args[])
+	{
+	Day4P0	obj = new Day4P0();
+	System.out.println(" ----- Package created successfully------------");
+
+	}
+
+
+}
