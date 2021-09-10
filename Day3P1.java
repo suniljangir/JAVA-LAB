55271510bd0e5f2a0b7e64777ad9240b78d9d35a
Dr. Sunil Kumar Jangir <39852522+suniljangir@users.noreply.github.com>
2021-09-10 17:50:53 +0530

    Add files via upload

---
+++ Day3P1.java
@@ -0,0 +1,19 @@
+// Single level inheritance
+
+class A				//Super class
+{
+int X=10;
+}
+
+class Day3P1 extends A		// sub/child class
+{
+int Y=20;
+	public static void main(String args[])
+	{
+		Day3P1  obj=new Day3P1  ();
+		System.out.println("X = "+obj.X+" Y = "+obj.Y);	// Accesing super class value using sub class object	
+	
+	}
+
+
+}
